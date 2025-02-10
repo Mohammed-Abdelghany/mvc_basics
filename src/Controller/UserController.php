cffcb0aff3c5acc1558e0d13a10e45ee97cb9db0
Muhammed Abd El-Ghany <64427688+Mohammed-Abdelghany@users.noreply.github.com>
2025-02-10 09:09:18 +0200

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Habiba\Session1\Controller;
+namespace Mvc\Session1\Controller;
 
-use Habiba\Session1\Model\User;
-use Habiba\Session1\View;
+use Mvc\Session1\Model\User;
+use Mvc\Session1\View;
 
 class UserController {
 
